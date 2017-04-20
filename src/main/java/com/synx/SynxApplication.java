5856c09e4c0b717479b45aa4669f13d27cd00a99
Mathieu-S <mathieusartorio@gmail.com>
2017-04-20 12:19:34 +0200

    Initial commit

---
+++ src/main/java/com/synx/SynxApplication.java
@@ -0,0 +1,12 @@
+package com.synx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynxApplication.class, args);
+	}
+}
